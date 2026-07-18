7b08179a1b5934d1f60a67a088fe34692bf90dd8
AishwaryaDV <millamatew@gmail.com>
2026-07-18 11:21:34 +0530

    Remove visible spinner from MapDynamic loading state

--- frontend/src/components/MapDynamic.tsx
+++ frontend/src/components/MapDynamic.tsx
@@ -3,11 +3,7 @@ import type { MapProps, MapMarker, MapHandle } from "./Map";
 
 const MapDynamic = dynamic(() => import("./Map"), {
   ssr: false,
-  loading: () => (
-    <div className="flex h-full w-full items-center justify-center bg-[#0b1120]">
-      <div className="h-5 w-5 animate-spin rounded-full border-2 border-zinc-700 border-t-blue-500" />
-    </div>
-  ),
+  loading: () => <div className="h-full w-full" />,
 });
 
 export default MapDynamic;
